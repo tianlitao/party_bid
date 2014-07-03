95871d338796608a455780da9dcd5e83bbbe0dc7
tianlitao <2423806988@qq.com>
2014-07-03 15:34:53 +0800

    开始结束按钮,一个活动报名开始后其它开始按钮不可点击

--- app/scripts/controllers/bidding_controller.js
+++ app/scripts/controllers/bidding_controller.js
@@ -2,59 +2,70 @@
  * Created by tlt on 14-6-21.
  */
 angular.module('angularApp')
-    .controller('BiddingCtrl', function ($scope,$location) {
+    .controller('BiddingCtrl', function ($scope, $location) {
 
-        $scope.back=function(){
-       $location.path('activity_list')
-   }
-        $scope.apply_status="2"
+        $scope.back = function () {
+            $location.path('activity_list')
+        }
+        var action = JSON.parse(localStorage.getItem("activities"));
+        for (var i in action)
+            if (action[i].name == localStorage.current_activity) {
+                $scope.applys = action[i].apply_list;
+            }
+        for (var j in action) {
+            if (action[j].name == localStorage.current_activity) {
+                if (action[j].activity_staus =='true') {
 
-        $scope.end=function(){
-           if(confirm("你确定结束吗"))
-            $scope.apply_status="2"
 
-                apply_last = "false"
-                var act = JSON.parse(localStorage.getItem("activities"));
-   //         console.log(localStorage.current_activity)
-           for(var i in act) {if(act[i].name==localStorage.current_activity)
-                act[i].activity_staus = apply_last
-                localStorage.setItem('activities', JSON.stringify(act))
+                    $scope.apply_status = "1"
+             //       $scope.disabled=true
+
+
+
+                } else {
+                    $scope.apply_status = "2"
+            //        $scope.disabled=true
+
+                }
             }
         }
 
-         var act= JSON.parse(localStorage.getItem("activities"));
-        for(var i in act)
-            if(act[i].name==localStorage.current_activity) {
-                $scope.applys = act[i].apply_list;
+        for(var i in action){
+            if(action[i].activity_staus=='true'){
+                $scope.disabled=true
+                break
+            }else{
+                $scope.disabled=false
             }
-//        console.log(act[0].apply_list[0].apply_name)
-//        for(var i in act){
-//
-//        }
+        }
 
+        $scope.end = function () {
+            if (confirm("你确定结束吗"))
+                $scope.apply_status = "2"
 
- //       console.log(typeof(act.name))
-
-//        var messages=act.apply_list
-//        console.log(messages)
-
-        $scope.begain=function() {
+            apply_last = "false"
+            //               var act = JSON.parse(localStorage.getItem("activities"));
+            //         console.log(localStorage.current_activity)
+            for (var i in action) {
+                if (action[i].name == localStorage.current_activity)
+                    action[i].activity_staus = apply_last
+                localStorage.setItem('activities', JSON.stringify(action))
+            }
+        }
+        $scope.begain = function () {
             $scope.apply_status = "1"
             apply_last = "true"
 
 
-            var act = JSON.parse(localStorage.getItem("activities"));
+            //                      var act = JSON.parse(localStorage.getItem("activities"));
 
 //            console.log(typeof(act[0].activity_staus))
 //            console.log(typeof(apply_last))
-            for (var i in act) {
-                if (act[i].name == localStorage.current_activity)
-                act[i].activity_staus = apply_last
-
-                    act[i].activity_staus = apply_last
-                localStorage.setItem('activities', JSON.stringify(act))
-
+            for (var i in action) {
+                if (action[i].name == localStorage.current_activity)
+                    action[i].activity_staus = apply_last
+                localStorage.setItem('activities', JSON.stringify(action))
             }
         }
-
-    });
+    }
+)
