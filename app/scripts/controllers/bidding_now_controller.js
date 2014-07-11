e4a98dea58b145df95e945c48aa3a2cd00645f2e
tianlitao <2423806988@qq.com>
2014-07-11 18:26:55 +0800

    进行活动竞价底色为黄色

--- app/scripts/controllers/bidding_now_controller.js
+++ app/scripts/controllers/bidding_now_controller.js
@@ -14,13 +14,13 @@ angular.module('angularApp')
         //   $scope.bid_start = true
 
         var bid = JSON.parse(localStorage.getItem("activities"));
-  for(var i in bid){
-      if(bid[i].bid_status=="false"){
-          $scope.bid_start=false
-      }else{
-          $scope.bid_start=true
-      }
-  }
+        for (var i in bid) {
+            if (bid[i].bid_status == "false") {
+                $scope.bid_start = false
+            } else {
+                $scope.bid_start = true
+            }
+        }
 
         for (var i in bid) {
             $scope.activity = bid[i].bid_list
@@ -33,6 +33,7 @@ angular.module('angularApp')
         $scope.bid = function (bidding) {
             $location.path('bidding_sign_up')
             localStorage.bid = bidding
+
         }
         $scope.back = function () {
             $location.path('activity_list')
@@ -41,7 +42,7 @@ angular.module('angularApp')
 
 
             for (var i in bid) {
-                bid[i].bid_status ="true"
+                bid[i].bid_status = "true"
                 localStorage.setItem("activities", JSON.stringify(bid))
                 $location.path('bidding_sign_up')
                 for (var i in bid) {
@@ -50,8 +51,9 @@ angular.module('angularApp')
                             var bid_name = "竞价1"
                         } else {
                             var bid_name = "竞价" + (bid[i].bid_list.length + 1)
+
                         }
-                        var bid_list = {'bid_name': bid_name}
+                        var bid_list = {'bid_name': bid_name,'bid_color':'true'}
                         bid[i].bid_list.unshift(bid_list)
                         localStorage.setItem('activities', JSON.stringify(bid))
                     }
--- app/scripts/controllers/bidding_sign_up_controller.js
+++ app/scripts/controllers/bidding_sign_up_controller.js
@@ -1,53 +1,35 @@
 angular.module('angularApp')
     .controller('Bidding_sign_upCtrl', function ($scope, $location) {
 
-        var bid=JSON.parse(localStorage.getItem("activities"))
-for(var i in bid){
-    if(bid[i].bid_status=="true"){
-        $scope.bid_start=false
+        var bid = JSON.parse(localStorage.getItem("activities"))
+        for (var i in bid) {
+            if (bid[i].bid_status == "true") {
+                $scope.bid_start = false
 
-    }else{
-        $scope.bid_start=true
-    }
-}
+            } else {
+                $scope.bid_start = true
+            }
+        }
+        $scope.end = function () {
+
+            for (var i in bid) {
+                bid[i].bid_status = "false"
+
+                localStorage.setItem("activities", JSON.stringify(bid))
+            }
+            for (var i in bid) {if(bid[i]==localStorage.getItem(current_)){
 
 
+            }
 
-//        if(localStorage.getItem("status")=="true"){
-//           $scope.bid_start=false
-//       }else{
-//           $scope.bid_start=true
-//       }
-
-
-
-        $scope.end=function(){
-
-            for(var i in bid){
-                bid[i].bid_status="false"
-                localStorage.setItem("activities",JSON.stringify(bid))
             }
             $location.path('bidding_now')
         }
 
 
-        $scope.back=function(){
+        $scope.back = function () {
             $location.path('bidding_now')
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
     })
