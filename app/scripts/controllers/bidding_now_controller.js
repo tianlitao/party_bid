90f56450e9c53a775458edb27007b3fd468876d9
tianlitao <2423806988@qq.com>
2014-07-11 16:39:53 +0800

    修改竞价页面开始结束按钮与报名页面的冲突

--- app/scripts/controllers/bidding_now_controller.js
+++ app/scripts/controllers/bidding_now_controller.js
@@ -14,37 +14,34 @@ angular.module('angularApp')
         //   $scope.bid_start = true
 
         var bid = JSON.parse(localStorage.getItem("activities"));
-        for(var i in bid){
-            $scope.activity=bid[i].bid_list
-        }
+  for(var i in bid){
+      if(bid[i].bid_status=="false"){
+          $scope.bid_start=false
+      }else{
+          $scope.bid_start=true
+      }
+  }
 
-        console.log(bid[0].bid_list)
+        for (var i in bid) {
+            $scope.activity = bid[i].bid_list
+        }
         for (var i in bid) {
             if (bid[i].activity_staus == 'true' || bid.length == 0) {
                 $scope.bid_start = true
             }
         }
-        $scope.bid=function(bidding){
+        $scope.bid = function (bidding) {
             $location.path('bidding_sign_up')
-            localStorage.bid=bidding
+            localStorage.bid = bidding
         }
         $scope.back = function () {
             $location.path('activity_list')
         }
-   //     $scope.bid_start=true
-        console.log(localStorage.getItem('status'))
-       if(localStorage.getItem("status")=="true"){
-            $scope.bid_start=true
-        }else{
-            $scope.bid_start=false
-        }
-
         $scope.begain = function () {
-            localStorage.status = "true"
 
-            var bid_status = 'true'
+
             for (var i in bid) {
-                bid[i].bid_status = bid_status
+                bid[i].bid_status ="true"
                 localStorage.setItem("activities", JSON.stringify(bid))
                 $location.path('bidding_sign_up')
                 for (var i in bid) {
@@ -52,7 +49,7 @@ angular.module('angularApp')
                         if (bid[i].bid_list.length == 0) {
                             var bid_name = "竞价1"
                         } else {
-                            var bid_name = "竞价"+(bid[i].bid_list.length + 1)
+                            var bid_name = "竞价" + (bid[i].bid_list.length + 1)
                         }
                         var bid_list = {'bid_name': bid_name}
                         bid[i].bid_list.unshift(bid_list)
--- app/scripts/controllers/bidding_sign_up_controller.js
+++ app/scripts/controllers/bidding_sign_up_controller.js
@@ -1,17 +1,33 @@
 angular.module('angularApp')
     .controller('Bidding_sign_upCtrl', function ($scope, $location) {
-       if(localStorage.getItem("status")=="true"){
-           $scope.bid_start=false
-       }else{
-           $scope.bid_start=true
-       }
+
+        var bid=JSON.parse(localStorage.getItem("activities"))
+for(var i in bid){
+    if(bid[i].bid_status=="true"){
+        $scope.bid_start=false
+
+    }else{
+        $scope.bid_start=true
+    }
+}
+
+
+
+//        if(localStorage.getItem("status")=="true"){
+//           $scope.bid_start=false
+//       }else{
+//           $scope.bid_start=true
+//       }
 
 
 
         $scope.end=function(){
-            $location.path('bidding_now')
-            localStorage.status="false"
 
+            for(var i in bid){
+                bid[i].bid_status="false"
+                localStorage.setItem("activities",JSON.stringify(bid))
+            }
+            $location.path('bidding_now')
         }
 
 
