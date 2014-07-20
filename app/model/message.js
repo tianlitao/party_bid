e16aba1c02d8e565c099e4fd3faf5ffceb15441b
tianlitao <2423806988@qq.com>
2014-07-20 21:55:53 +0800

    sms收发短信的重构

---
+++ app/model/message.js
@@ -0,0 +1,120 @@
+/**
+ * Created by tlt on 14-7-20.
+ */
+function Message() {
+
+}
+Message.check_activity_status_bm = function (json_message) {
+    var act_list = JSON.parse(localStorage.getItem("activities"));
+    var message = json_message.messages[0].message.replace(/\s/g, "");
+    var apply_phone = json_message.messages[0].phone
+    if (message.search(/bm/i) == 0) {
+        var act = _.findWhere(act_list, {activity_status: 'true'}).apply_list
+        return( _.find(act, function (act) {
+            return act.apply_phone == apply_phone
+        }))
+
+    }
+}
+Message.save_message = function (json_message) {
+    var act_list = JSON.parse(localStorage.getItem("activities"));
+    var message = json_message.messages[0].message.replace(/\s/g, "");
+    var apply_name = message.substr(2).trim()
+    var apply_phone = json_message.messages[0].phone
+    var apply_array = {'apply_name': apply_name, 'apply_phone': apply_phone}
+    _.findWhere(act_list, {activity_status: 'true'}).apply_list.push(apply_array)
+    localStorage.setItem("activities", JSON.stringify(act_list))
+
+}
+Message.check_activity_status = function (json_message) {
+    var act_list = JSON.parse(localStorage.getItem("activities"));
+    var message = json_message.messages[0].message.replace(/\s/g, "");
+    return(_.find(act_list, function (act) {
+        return act.activity_status == 'true' && message.search(/bm/i) == 0
+    }))
+}
+Message.refresh = function () {
+    var refresh_page = document.getElementById('sign_up_page_id')
+    if (refresh_page) {
+        var scope = angular.element(refresh_page).scope();
+        scope.$apply(function () {
+            scope.diaoyong();
+        })
+    }
+}
+Message.check_message_jj = function (json_message) {
+    var act_list = JSON.parse(localStorage.getItem("activities"));
+    var message = json_message.messages[0].message.replace(/\s/g, "");
+    return( _.find(act_list, function (act) {
+        return act.bid_status == 'true' && message.search(/jj/i) == 0
+    }))
+}
+Message.check_message_phone = function (json_message) {
+    var activity = Message.check_message_jj(json_message)
+    var bid_phone = json_message.messages[0].phone
+    var act = activity.apply_list
+    return(_.find(act, function (apply) {
+        return apply.apply_phone == bid_phone
+    }))
+}
+Message.refresh_bid=function(){
+    var refresh_page = document.getElementById('page_id')
+    if (refresh_page) {
+        var scope = angular.element(refresh_page).scope();
+        scope.$apply(function () {
+            scope.break();
+        })
+    }
+}
+Message.save_bid_message=function(json_message){
+    var act_list = JSON.parse(localStorage.getItem("activities"));
+
+    var message = json_message.messages[0].message.replace(/\s/g, "");
+    var activity= _.find(act_list, function (act) {
+        return act.bid_status == 'true' && message.search(/jj/i) == 0
+    })
+    var bid_price = message.substr(2)
+    var bid_phone = json_message.messages[0].phone
+    var even=Message.check_message_phone(json_message)
+    var bid_name = even.apply_name
+    var bid_message = {'bid_price': bid_price, 'bid_phone': bid_phone, 'bid_name': bid_name} || []
+    console.log("1")
+    activity.bid_list[0].bid_message.push(bid_message)
+    localStorage.setItem('activities', JSON.stringify(act_list))
+
+
+
+
+}
+Message.check_bid_phone=function(json_message){
+    var activity=Message.check_message_jj(json_message)
+    var bid_phone = json_message.messages[0].phone
+    var bid = activity.bid_list[0].bid_message
+    return( _.find(bid, function (bid) {
+        return bid.bid_phone == bid_phone
+    }))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/scripts/sms.js
+++ app/scripts/sms.js
@@ -7,91 +7,33 @@ var native_accessor = {
 //        ]}, {"message_content": message});
         console.log(phone, message);
     },
-
     receive_message: function (json_message) {
         if (typeof this.process_received_message === 'function') {
             this.process_received_message(json_message);
         }
     },
     process_received_message: function (json_message) {
-        var act_list = JSON.parse(localStorage.getItem("activities"));
-        var message = json_message.messages[0].message.replace(/\s/g, "");
-        //     console.log(act_list[0].apply_list.length)
-        var action_list = _.find(act_list, function (act) {
-            return act.activity_status == 'true' && message.search(/bm/i) == 0
-        })
-        if (action_list) {
-            var apply_name = message.substr(2).trim()
-            var apply_phone = json_message.messages[0].phone
-            var apply_array = {'apply_name': apply_name, 'apply_phone': apply_phone}
-            for (var j = 0; j < action_list.apply_list.length; j++) {
-                if (apply_phone == action_list.apply_list[j].apply_phone) {
-                    native_accessor.send_sms(json_message.messages[0].phone, "您已报名成功，请勿重复报名")
-                    return
-                }
-            }
-            action_list.apply_list.push(apply_array)
-            localStorage.setItem('activities', JSON.stringify(act_list))
+        if (Message.check_activity_status_bm(json_message)) {
+            native_accessor.send_sms(json_message.messages[0].phone, "您已报名成功，请勿重复报名")
+            return;
+        }
+        if (Message.check_activity_status(json_message) && !Message.check_activity_status_bm(json_message)) {
+            Message.save_message(json_message)
             native_accessor.send_sms(json_message.messages[0].phone, "恭喜您已报名成功")
-            sign_up_page_refresh()
-            function sign_up_page_refresh() {
-                var refresh_page = document.getElementById('sign_up_page_id')
-                if (refresh_page) {
-                    var scope = angular.element(refresh_page).scope();
-                    scope.$apply(function () {
-                        scope.diaoyong();
-                    })
-                }
-
-            }
+            Message.refresh()
+            return;
         }
-        var activity = _.find(act_list, function (act) {
-            return act.bid_status == 'true' && message.search(/jj/i) == 0
-        })
-        if (activity) {
-            var bid_price = message.substr(2)
-            var bid_phone = json_message.messages[0].phone
-            var act = activity.apply_list
-            var bid = activity.bid_list[0].bid_message
-            var even = _.find(act, function (apply) {
-                return apply.apply_phone == bid_phone
-            })
-            function refresh() {
-                var refresh_page = document.getElementById('page_id')
-                if (refresh_page) {
-                    var scope = angular.element(refresh_page).scope();
-                    scope.$apply(function () {
-                        scope.break();
-                    })
-                }
-            }
-            if (even) {
-                var bid_name = even.apply_name
-                var bid_message = {'bid_price': bid_price, 'bid_phone': bid_phone, 'bid_name': bid_name} || []
-                if (activity.bid_list[0].bid_message.length == 0) {
-                    console.log("3")
-                    activity.bid_list[0].bid_message.push(bid_message)
-                    localStorage.setItem('activities', JSON.stringify(act_list))
-                    native_accessor.send_sms(json_message.messages[0].phone, "恭喜您已竞价成功")
-                    refresh()
-                }
-                if (_.find(bid, function (bid) {
-                    return bid.bid_phone == bid_phone
-                })) {
-                    native_accessor.send_sms(json_message.messages[0].phone, "请勿重复竞价")
-                    return
-                } else {
-                    console.log(bid_message)
-                    activity.bid_list[0].bid_message.push(bid_message)
-                    localStorage.setItem('activities', JSON.stringify(act_list))
-                    native_accessor.send_sms(json_message.messages[0].phone, "恭喜您已竞价成功")
-                    refresh()
-                }
-
-//            } else {
-//                native_accessor.send_sms(json_message.messages[0].phone, "对不起,您没有报名此次活动")
-            }
+        if (Message.check_message_phone(json_message) && Message.check_message_jj(json_message) && Message.check_bid_phone(json_message)) {
+            native_accessor.send_sms(json_message.messages[0].phone, "请勿重复竞价")
+            return
         }
+        if (Message.check_message_phone(json_message) && Message.check_message_jj(json_message)) {
+            Message.save_bid_message(json_message)
+            native_accessor.send_sms(json_message.messages[0].phone, "恭喜您已竞价成功")
+            Message.refresh_bid()
+            return
+        }
+
     }
 }
 
